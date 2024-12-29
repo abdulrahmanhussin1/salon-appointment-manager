2a95f1f2b7539bea8bbc39d1ef620c3c62cd438d
Amgad-Anwar <amgadanwar01277112438@gmail.com>
2024-12-29 20:19:40 +0200

    calender

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AppointmentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\ToolController;
