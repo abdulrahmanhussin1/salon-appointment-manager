eafeed18e8262ca8b5e62163f29172f260fb7cf6
Abdulrahman Hussin <abdelrahman.hussin1@gmail.com>
2025-01-07 01:39:31 +0200

    make seeders

--- database/seeders/EmployeeLevelSeeder.php
+++ database/seeders/EmployeeLevelSeeder.php
@@ -13,6 +13,6 @@ class EmployeeLevelSeeder extends Seeder
      */
     public function run(): void
     {
-        //EmployeeLevel::factory()->count(5)->create();
+        EmployeeLevel::factory()->count(5)->create();
     }
 }
